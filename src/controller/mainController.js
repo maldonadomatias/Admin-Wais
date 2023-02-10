da84807853d6843b5256cc56137173b077c89bcd
Matias Agustin Maldonado <molporron@gmail.com>
2023-02-10 13:33:36 -0300

    Add variable

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,62 +1,62 @@
-const nodemailer = require("nodemailer");
-const { db, auth } = require("../firebase");
-const { genPassword } = require("../util/functions");
+const nodemailer = require('nodemailer')
+const { db, auth } = require('../firebase')
+const { genPassword } = require('../util/functions')
 
 const mainController = {
   index: (req, res) => {
-    res.render("index");
+    res.render('index')
   },
   thanks: (req, res) => {
-    res.render("thanks");
+    res.render('thanks')
   },
   mail: async (req, res) => {
-    const { name, email, role } = req.body;
-    const password = genPassword();
+    const { name, email, role } = req.body
+    const password = genPassword()
 
-    await db.collection("users").add({
+    await db.collection('users').add({
       created: Date.now(),
       displayName: name,
       role: role,
       user: email,
-    });
+    })
 
     try {
       await auth.createUser({
         email: email,
         password: password,
-      });
+      })
 
       let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-          user: process.env.EMAIL, // generated ethereal user
-          pass: process.env.PASSWORD, // generated ethereal password
+          user: 'maldonadomatiasagustin@gmail.com', // generated ethereal user
+          pass: 'ctccjntaiodadpzu', // generated ethereal password
         },
-      });
+      })
 
       try {
         await transporter.sendMail({
-          from: "apolo@services.com", // sender address
-          subject: "Nuevo Usuario",
+          from: 'apolo@services.com', // sender address
+          subject: 'Nuevo Usuario',
           to: email, // list of receivers
           text: `Hola, bienvenido a Apolo Oil Services. \n
           Estos son tus datos:\n
           -Mail: ${email}\n
           -Contraseña: ${password}\n`, // plain text body
-        });
+        })
       } catch (e) {
-        console.log(e, "Error!");
+        console.log(e, 'Error!')
       }
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-    res.redirect("/");
+    res.redirect('/')
   },
   error: (req, res) => {
-    res.render("error");
+    res.render('error')
   },
-};
+}
 
-module.exports = mainController;
+module.exports = mainController
